a26b7c34535d66e6ba13aeb716531178afa6f91b
davidMarabottini <david.marabottini@gmail.com>
2026-08-05 20:50:28 +0200

    feat: introduced duplicated list view for employees

--- src/pages/Employees/List/List.tsx
+++ src/pages/Employees/List/List.tsx
@@ -3,15 +3,18 @@ import Typography from "@components/atoms/Typography/Typography";
 import styles from './List.module.scss'
 import type { LibemaxEmployee } from "@/api/types";
 import { ROUTES } from "@/constants/routes";
-import {  PlusCircle } from "lucide-react";
+import {  Edit2, Eye, PlusCircle, Trash2 } from "lucide-react";
 import LinkComponent from "@/components/atoms/LinkComponent/LinkComponent";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { DeleteModal } from "./components/DeleteModal/DeleteModal";
 import Paginated from "@/components/organisms/Paginated/Paginated";
 import { useEmployeesList } from "@/hooks/api/useEmployeesHooks";
-// import EmployeeDetailCard from "./components/EmployeeDetailCard/EmployeeDetailCard";
 import EmployeeDetailsCard from "@/components/molecules/DetailCards/EmployeeDetailsCard/EmployeeDetailsCard";
+import { useViewStore } from "@/zustand/listViewAsCard";
+import { rewriteRoute } from "@/utils/routes";
+import Button from "@/components/atoms/Button/Button";
+import Table from "@/components/organisms/Table/Table";
 
 const LibemaxEmployees = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
@@ -49,19 +52,48 @@ const LibemaxEmployees = () => {
             { key: 'email', placeholder: '', label: 'Cerca Email' },
           ]}
         >
-          {(res) => (
-            <div className={styles["p-libemax-employees__grid"]}>
-              {
-                res.map((employee) =>
+          {(res) => {
+            const isCardView = useViewStore.getState().isCardView;
+            return isCardView ? (
+              <div className={styles["p-libemax-employees__grid"]}>
+                {res.map((employee) => (
                   <EmployeeDetailsCard
                     key={employee.libemax_id}
                     employee={employee}
                     toggleDelete={openDeleteModalHdlr}
                   />
-                )
-              }
-            </div>
-          )}
+                ))}
+              </div>
+            ) : (
+              <Table<LibemaxEmployee>
+                data={res}
+                columns={[
+                  {key: "libemax_id", header: t("table.libemaxId")},
+                  {key: "name", header: t("table.name") },
+                  {key: "email", header: t("table.email") },
+                  {key: "phone", header: t("table.phone")},
+                  {key: "email", header: t("table.email")},
+                  
+                ]}
+                 actions={[
+          ({id}) => <LinkComponent key="details" to={rewriteRoute(ROUTES.EMPLOYEE_DETAIL, {':employeeId': id.toString()})}>
+            <Eye />
+          </LinkComponent>,
+          ({id}) => <LinkComponent key="edit" to={rewriteRoute(ROUTES.EDIT_EMPLOYEE, { ':idEmployee': id.toString() })}>
+            <Edit2 />
+          </LinkComponent>,
+          (employee) => <Button
+          key="remove"
+          color="custom"
+          additionalClassName={styles["c-employees-details-card__btn-delete"]}
+          onClick={() => openDeleteModalHdlr(employee)}
+        >
+          <Trash2 />
+        </Button>
+      ]}
+              />
+            )
+          }}
         </Paginated>
       </Card>
     </div>
